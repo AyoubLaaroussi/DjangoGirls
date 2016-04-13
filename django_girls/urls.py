67d02d5a9bfeeeae41db30c6191f9e757115f0a6
AyoubLaaroussi <1smr1.laaroussi.ayoub@gmail.com>
2016-04-13 23:33:12 +0200

    logeo correcto

--- django_girls/urls.py
+++ django_girls/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'', include('blog.urls')),
     url(r'^logout/$','django.contrib.auth.views.logout',{'next_page': '/'}),
+    url(r'^login/$','django.contrib.auth.views.login',{'template_name': 'base.html'}),
     url('^',include('django.contrib.auth.urls')),
 ]
