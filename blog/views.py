0b744c4fc5b06130bb1a4cc21472e1caa96f7d30
AyoubLaaroussi <1smr1.laaroussi.ayoub@gmail.com>
2016-04-15 13:46:13 +0200

    nos vamos de viernes

--- blog/views.py
+++ blog/views.py
@@ -1,52 +1,86 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
+from django.core.urlresolvers import reverse_lazy
 from .models import Post, Comment
 from .forms import PostForm, CommentForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate
 
+from django.views.generic import ListView, DetailView, CreateView
 
-def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+
+#def post_list(request):
+#    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     #import ipdb; ipdb.set_trace()
-    return render(request, 'blog/post_list.html', {'posts': posts})
+#    return render(request, 'blog/post_list.html', {'posts': posts})
 
-def post_detail(request, pk): 
-    post = get_object_or_404(Post, pk=pk) 
-    comments = Comment.objects.filter(post=post).order_by('created_date')
+class PostListView(ListView):
 
-    if request.method == "POST":
-        form = CommentForm(request.POST)
+    context_object_name = 'posts'
+    def get_queryset(self):
+        queryset =  Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+        return queryset
+    
 
-        if form.is_valid():
-            if request.user.is_authenticated():
-                comment = form.save(commit=False)
-                comment.author = request.user
-                comment.post = post
-                comment.save()  
-                return redirect('post_detail', pk=post.pk)
-            else:
-                return redirect('login')   
-        return redirect('post_detail', pk=post.pk)
-    else:
-        form = CommentForm()
+class PostDetail(DetailView):
+    model = Post
 
-    return render(request, 'blog/post_detail.html', {'post': post, 'comments':comments,'form':form})
+class PostCreate(CreateView):
+    model = Post
+    fields = ['title','text']
+    template_name = 'blog/post_edit.html'
 
-@login_required(login_url='login')
-def post_new(request):
-    if request.method == "POST":
-        form = PostForm(request.POST)
+    def form_valid(self, form):
+        post = form.instance
+        post.published_date = timezone.now()
+        post.author = self.request.user
+        return super(PostCreate, self).form_valid(form)
 
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.author = request.user
-            post.publish()
+    success_url = "/"
+
+        
+
+
+
+
+
+
+# def post_detail(request, pk): 
+#     post = get_object_or_404(Post, pk=pk) 
+#     comments = Comment.objects.filter(post=post).order_by('created_date')
+
+#     if request.method == "POST":
+#         form = CommentForm(request.POST)
+
+#         if form.is_valid():
+#             if request.user.is_authenticated():
+#                 comment = form.save(commit=False)
+#                 comment.author = request.user
+#                 comment.post = post
+#                 comment.save()  
+#                 return redirect('post_detail', pk=post.pk)
+#             else:
+#                 return redirect('login')   
+#         return redirect('post_detail', pk=post.pk)
+#     else:
+#         form = CommentForm()
+
+#     return render(request, 'blog/post_detail.html', {'post': post, 'comments':comments,'form':form})
+
+# @login_required(login_url='login')
+# def post_new(request):
+#     if request.method == "POST":
+#         form = PostForm(request.POST)
+
+#         if form.is_valid():
+#             post = form.save(commit=False)
+#             post.author = request.user
+#             post.publish()
             
-            return redirect('post_detail', pk=post.pk)
-    else:
-        form = PostForm()
-    return render(request, 'blog/post_edit.html', {'form': form})
+#             return redirect('post_detail', pk=post.pk)
+#     else:
+#         form = PostForm()
+#     return render(request, 'blog/post_edit.html', {'form': form})
 
 @login_required(login_url='login')
 def post_edit(request, pk):
