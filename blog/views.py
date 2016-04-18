65c515ddc876a9d6bbebe2f4d82fe916f52bdea7
AyoubLaaroussi <1smr1.laaroussi.ayoub@gmail.com>
2016-04-18 13:41:12 +0200

    subida final

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from .forms import CommentForm
 from django.utils import timezone
 from django.core.urlresolvers import reverse
 from .models import Post, Comment
-from .forms import PostForm, CommentForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate
-
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
 
@@ -31,7 +30,28 @@ class PostListView(ListView):
 
 class PostDetail(DetailView):
     model = Post
-
+    def get_context_data(self, **kwargs):
+        context = super(PostDetail, self).get_context_data(**kwargs)
+        context['comments'] = Comment.objects.filter(post=self.object).order_by('created_date')
+        context['form'] = CommentForm
+        return context
+        
+    def post(self, request, *args, **kwargs):
+        import ipdb; ipdb.set_trace()
+        if request.user.is_authenticated():
+            form = CommentForm(request.POST)
+            post = self.get_object()
+            if form.is_valid() :
+                comment = form.save(commit=False)
+                comment.author = request.user
+                comment.post = post
+                comment.save()  
+                
+                return redirect('post_detail', pk=post.pk)
+        else:
+            return redirect('login')   
+        
+   
 class PostCreate(CreateView):
     model = Post
     fields = ['title','text']
